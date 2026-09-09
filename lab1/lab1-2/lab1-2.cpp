b6b45c1eadd3dd55a1ab521426b3f846f76b03a2
mot <heisenburger06@gmail.com>
2026-09-10 00:43:18 +0300

    added lab1 (1-4)

---
+++ lab1/lab1-2/lab1-2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+    double x, y, z;
+    std::cout << "Enter x: ";
+    std::cin >> x;
+    std::cout << "Enter y: ";
+    std::cin >> y;
+    std::cout << "Enter z: ";
+    std::cin >> z;
+
+    double a = std::pow(9+std::pow(x-y,2), 1.0 / 3.0);
+    double b = std::pow(x, 2) + std::pow(y, 2) + 2.0;
+    double c = std::exp(std::abs(x-y))*std::pow(std::tan(z),3);
+    double s = a / b - c;
+    std::cout << "Result s = " << s << std::endl;
+    return 0;
+}
---
+++ lab1/lab1-3/lab1-3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+	double x, y, z;
+	std::cout << "Enter x: ";
+	std::cin >> x;
+	std::cout << "Enter y: ";
+	std::cin >> y;
+	std::cout << "Enter z: ";
+	std::cin >> z;
+
+	double a = 1.0+std::pow(std::sin(x+y),2);
+	double d = std::pow(x,std::abs(y));
+	double b = std::abs(x-(2.0*y/(1.0+std::pow(x*y,2))));
+	double c = std::pow(std::cos(std::atan(1.0/z)),2);
+	double s = (a / b) * d + c;
+	std::cout << "Result s = " << s << std::endl;
+	return 0;
+}

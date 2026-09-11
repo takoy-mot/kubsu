0b3b767e0563b05226e2da70ca49499f4e4b7d78
mot <heisenburger06@gmail.com>
2026-09-12 01:19:02 +0300

    try to change .gitignore 6

---
+++ lab1/lab1-4/lab1-4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+	double x, y, z;
+	std::cout << "Enter x: ";
+	std::cin >> x;
+	std::cout << "Enter y: ";
+	std::cin >> y;
+	std::cout << "Enter z: ";
+	std::cin >> z;
+
+	double a = std::pow(std::abs(std::cos(x)-std::cos(y)),(1.0+2.0*std::pow(std::sin(y),2)));
+	double b = 1+z+std::pow(z,2)/2 + std::pow(z, 3) / 3.0 + std::pow(z, 4) / 4.0;
+	double s = a * b;
+	std::cout << "Result s = " << s << std::endl;
+	return 0;
+}
